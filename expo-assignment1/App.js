ed16faa3db3326da10360308c430967cdf1192e8
JamesSelikem <jamesselikem1@gmail.com>
2024-05-26 23:08:47 +0000

    font size of text increased to 24

--- expo-assignment1/App.js
+++ expo-assignment1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is James</Text>
+      <Text style={styles.text}>My name is James</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24, // Increase font size to 24
   },
 });
